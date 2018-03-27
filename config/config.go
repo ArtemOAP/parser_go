24b9ade6626de7f37f83510cde6de2eb98258150
Artem O <dev@mail.ru>
2018-03-27 18:47:48 +0700

    request

--- config/config.go
+++ config/config.go
@@ -4,40 +4,38 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+
 	"gopkg.in/yaml.v2"
 )
 
 type Config struct {
 	Parser struct {
-		TimeSleep    int64 `yaml:"timeSleep"`
-		Gol          uint
+		TimeSleep    int `yaml:"timeSleep"`
+		Gol          int
 		RootDir      string `yaml:"rootDir"`
 		HrefAllLinks string `yaml:"hrefAllLinks"`
 		MobAgent     string `yaml:"mobAgent"`
 		DescAgent    string `yaml:"descAgent"`
-		Links        []string
+		Links        map[string]string
 		CountLink    int `yaml:"countLink"`
 		Index        string
 		Script       string
 		NotIframe    bool `yaml:"notIframe"`
 		Ajax         bool
+		Dir          string
 	}
 }
 
-func GetConfig() {
-
+func GetConfig() *Config {
 	var conf Config
 	source, err := ioutil.ReadFile("./config.yml")
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(string(source))
 	errY := yaml.Unmarshal(source, &conf)
 	if errY != nil {
 		log.Fatalf("error: %v", err)
 	}
-
 	fmt.Printf("--- config:\n%v\n\n", conf)
-
-	fmt.Println(conf.Parser.Links)
+	return &conf
 }
--- parser.go
+++ parser.go
@@ -9,14 +9,13 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"parser_go/config"
 	"regexp"
 	"strconv"
 	"strings"
-	"github.com/PuerkitoBio/goquery"
-	"parser_go/config"
-)
 
-const DIR_SITE = "sites/"
+	"github.com/PuerkitoBio/goquery"
+)
 
 type parserOnePage struct {
 	timeSleep      int
@@ -32,7 +31,7 @@ type parserOnePage struct {
 	dirAgent       string
 	mobAgent       string
 	descAgent      string
-	links          []string
+	links          map[string]string
 	countLink      int
 	dirs           map[string]string
 	indFile        string
@@ -50,48 +49,38 @@ const (
 )
 
 var instance *parserOnePage
-
 var ch chan int
-
 var chName chan string
 var chUrl chan string
 var chTypeImages chan bool
 var chTypeSrc chan bool
 
-
-
 func main() {
-	config.GetConfig()
-	
-
-	// parser := getInstance()
-	// parser.setTempName("test")
-	// parser.baseLink = "https://toster.ru/q/431978"
-	// fmt.Println("start")
-	// ch = make(chan int)
-	// chName = make(chan string)
-	// chUrl = make(chan string)
-	// chTypeImages = make(chan bool)
-	// chTypeSrc = make(chan bool)
-	// parser.run()
+	parser := getInstance(config.GetConfig())
+	fmt.Println("start")
+	fmt.Println(parser.links)
+	parser.run()
 }
 
 func (p *parserOnePage) run() *parserOnePage {
 
-	baseLink := getInstance().baseLink
-	if baseLink == "" {
-		log.Fatal("Not init base links")
+	if len(p.links) < 1 {
+		log.Fatal("Not init link(s)")
 	}
-	//TODO baseLink to one
-	message("parse start", "keep", strconv.Itoa(p.countLink), "page(s)")
-	p.setOptions()
-	p.parsePage(baseLink)
+	message("parse start", "keep", strconv.Itoa(len(p.links)), "page(s)")
+
+	for name, link := range p.links {
+		p.baseLink = link
+		p.setTempName(name)
+		p.setOptions()
+		p.parsePage(link)
+	}
 
 	return p
 }
 func (p *parserOnePage) parsePage(link string) {
-
-	res, err := http.Get(link)
+	err, res := p.request(link)
+	defer res.Body.Close()
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -99,7 +88,6 @@ func (p *parserOnePage) parsePage(link string) {
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	p.thenBaseHref(doc)
 	p.saveIco(doc)
 	p.saveModifyElemHref(doc)
@@ -139,7 +127,7 @@ func (p *parserOnePage) parsePage(link string) {
 	        $this->add($page,'<script type="text/javascript" src="/js/script.js"></script>');
 		**/
 
-	p.save(doc, ".")
+	p.save(doc, p.rootDir+"/"+p.tempName+"/")
 	message("pages url=", p.baseLink)
 	message("OK All create")
 
@@ -173,7 +161,7 @@ func (p *parserOnePage) saveIco(doc *goquery.Document) {
 	doc.Find("link[rel=icon],link[rel=apple-touch-icon]").Each(func(i int, s *goquery.Selection) {
 		link, _ := s.Attr("href")
 		if link != "" {
-			link = urlAbsolute(link, p.baseLink)
+			link = p.urlAbsolute(link, p.baseLink)
 			name := p.filterFileName(link)
 			p.saveFile(link, p.dirs["img"], name)
 			s.SetAttr("href", p.dirs["img_r"]+name)
@@ -216,7 +204,7 @@ func (p *parserOnePage) saveModifyJs(doc *goquery.Document) {
 				s.SetAttr("src", p.dirs["js_r"]+name)
 				if _, err := os.Stat(p.dirs["js"] + name); os.IsNotExist(err) {
 
-					link := urlAbsolute(src, p.baseLink)
+					link := p.urlAbsolute(src, p.baseLink)
 					_, response := p.request(link)
 					defer response.Body.Close()
 					if response != nil {
@@ -254,7 +242,7 @@ func (p *parserOnePage) request(url string) (error, *http.Response) {
 	}
 	resp, err := client.Do(req)
 
-	if err != nil || resp.StatusCode != 200 {
+	if err != nil && (err == nil && resp.StatusCode != 200) {
 		//TODO log
 		return err, nil
 	}
@@ -280,7 +268,7 @@ func (p *parserOnePage) saveModifayCss(doc *goquery.Document) {
 				response.Body.Close()
 				name := p.filterFileName(href)
 
-				linkCss := urlAbsolute(href, p.baseLink)
+				linkCss := p.urlAbsolute(href, p.baseLink)
 				linkDirCss := substrFind(linkCss, '/')
 				result_replace_url := reg3.ReplaceAllStringFunc(string(bodyb), func(str string) string {
 					return "url(" + p.getfileSrc(str, linkDirCss, false) + ")"
@@ -314,7 +302,7 @@ func (p *parserOnePage) saveModifyImg(doc *goquery.Document) map[string]string {
 			if p.temp_files[name] == "" {
 				name = "img-" + name
 			}
-			p.temp_files[name] = urlAbsolute(link, p.baseLink)
+			p.temp_files[name] = p.urlAbsolute(link, p.baseLink)
 
 		}
 		s.SetAttr("src", p.dirs["img_r"]+name)
@@ -334,7 +322,7 @@ func (p *parserOnePage) getfileSrc(link string, url string, at_html bool) string
 	connectLink := func() string {
 		reg := regexp.MustCompile(`/^htt(p|ps):/`)
 		if !reg.MatchString(newlink) {
-			return urlAbsolute(newlink, url)
+			return p.urlAbsolute(newlink, url)
 		}
 		return newlink
 	}
@@ -493,33 +481,38 @@ func (p *parserOnePage) CreateDirIfNotExist(dir string) {
 	}
 }
 
-func getInstance() *parserOnePage {
+func getInstance(conf *config.Config) *parserOnePage {
 
 	if instance == nil {
 		instance = &parserOnePage{
-			timeSleep:      10,
-			gol:            2,
+			timeSleep:      conf.Parser.TimeSleep,
+			gol:            conf.Parser.Gol,
 			temp_files_src: make(map[string]string),
 			temp_files:     make(map[string]string),
-			rootDir:        ".",
-			hrefAllLinks:   "#",
-			mobAgent:       "Mozilla/5.0 (Linux; U; Android 4.0.3; ko-kr; LG-L160L Build/IML74K) AppleWebkit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30",
-			descAgent:      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
-			links:          make([]string, 0),
-			countLink:      1,
+			rootDir:        conf.Parser.RootDir,
+			hrefAllLinks:   conf.Parser.HrefAllLinks,
+			mobAgent:       conf.Parser.MobAgent,
+			descAgent:      conf.Parser.DescAgent,
+			links:          conf.Parser.Links,
+			countLink:      conf.Parser.CountLink,
 			dirs:           make(map[string]string),
-			index:          "index.php",
-			script:         "<?php  $useragent=$_SERVER['HTTP_USER_AGENT'];   if(preg_match('/(android|bb\\d+|meego).+mobile|avantgo|bada\\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i',$useragent)||preg_match('/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\\-(n|u)|c55\\/|capi|ccwa|cdm\\-|cell|chtm|cldc|cmd\\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\\-s|devi|dica|dmob|do(c|p)o|ds(12|\\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\\-|_)|g1 u|g560|gene|gf\\-5|g\\-mo|go(\\.w|od)|gr(ad|un)|haie|hcit|hd\\-(m|p|t)|hei\\-|hi(pt|ta)|hp( i|ip)|hs\\-c|ht(c(\\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\\-(20|go|ma)|i230|iac( |\\-|\\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\\/)|klon|kpt |kwc\\-|kyo(c|k)|le(no|xi)|lg( g|\\/(k|l|u)|50|54|\\-[a-w])|libw|lynx|m1\\-w|m3ga|m50\\/|ma(te|ui|xo)|mc(01|21|ca)|m\\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\\-2|po(ck|rt|se)|prox|psio|pt\\-g|qa\\-a|qc(07|12|21|32|60|\\-[2-7]|i\\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\\-|oo|p\\-)|sdk\\/|se(c(\\-|0|1)|47|mc|nd|ri)|sgh\\-|shar|sie(\\-|m)|sk\\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\\-|v\\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\\-|tdg\\-|tel(i|m)|tim\\-|t\\-mo|to(pl|sh)|ts(70|m\\-|m3|m5)|tx\\-9|up(\\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\\-|your|zeto|zte\\-/i',substr($useragent,0,4)))  {      require_once ('index2.php');      die();  }  ?>",
-			notIframe:      true,
-			ajax:           false,
-			dir:            DIR_SITE,
+			index:          conf.Parser.Index,
+			script:         conf.Parser.Script,
+			notIframe:      conf.Parser.NotIframe,
+			ajax:           conf.Parser.Ajax,
+			dir:            conf.Parser.Dir,
 		}
+		ch = make(chan int)
+		chName = make(chan string)
+		chUrl = make(chan string)
+		chTypeImages = make(chan bool)
+		chTypeSrc = make(chan bool)
 	}
 
 	return instance
 }
 
-func urlAbsolute(link string, baseLink string) string {
+func (p *parserOnePage) urlAbsolute(link string, baseLink string) string {
 
 	reg1, _ := regexp.Compile("^htt(p|ps)://")
 	reg2, _ := regexp.Compile("^//")
@@ -529,8 +522,8 @@ func urlAbsolute(link string, baseLink string) string {
 	if reg1.MatchString(link) && reg1.MatchString(baseLink) {
 		return link
 	}
-	if getInstance().baseTeg != "" {
-		link = getInstance().baseTeg + link
+	if p.baseTeg != "" {
+		link = p.baseTeg + link
 	}
 	u, err := url.Parse(baseLink)
 	if err != nil {
